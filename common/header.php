cdcf28a172ae7a78d99a6093aeb08a4e29e2b804
masanari9256 <masanari92562@gmail.com>
2022-01-22 14:10:29 +0900

    [fix]DB接続関数を修正

--- common/header.php
+++ common/header.php
@@ -18,10 +18,15 @@ EOF;
 
 function getDatabaseConnection() {
   try {
-    $database_handler = new PDO('mysql:host=db;dbname=simple_memo;charset=ith8mb4', 'root', 'password');
+    $database_handler = new PDO(
+      'mysql:host=db;dbname=simple_memo;charset=utf8mb4',
+      'root',
+      'password'
+    );
   } catch(PDOException $e) {
     echo '接続に失敗しました';
     echo $e->getMessage();
     exit;
   }
+  return $database_handler;
 }
