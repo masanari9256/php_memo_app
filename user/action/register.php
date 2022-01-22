ecf01daa089156f57e10e7652708e33a3e52370f
masanari9256 <masanari92562@gmail.com>
2022-01-22 14:10:51 +0900

    [update]ユーザー登録機能を実装

--- user/action/register.php
+++ user/action/register.php
@@ -1,1 +1,27 @@
 <?php
+require '../../common/header.php';
+
+$user_name = $_POST['user_name'];
+$user_email = $_POST['user_email'];
+$user_password = $_POST['user_password'];
+
+$database_handler = getDatabaseConnection();
+
+try {
+  if($statement = $database_handler->prepare(
+    'INSERT INTO users (name, email, password) VALUES (:name, :email, :password)'
+  )) {
+    $password = password_hash($user_password, PASSWORD_DEFAULT);
+  
+    $statement->bindParam(':name', htmlspecialchars($user_name));
+    $statement->bindParam(':email', $user_email);
+    $statement->bindParam(':password', $password);
+    $statement->execute();
+  }
+} catch(Throwable $e) {
+  echo $e->getMessage();
+  exit;
+}
+
+header('Location: ../../memo/');
+exit;
