4a28f57934cd63e89d1bca49c51d18c6f1e26e7c
masanari9256 <masanari92562@gmail.com>
2022-01-23 15:23:29 +0900

    [update]ユーザー登録にバリデーションを実装

--- user/action/register.php
+++ user/action/register.php
@@ -1,9 +1,41 @@
 <?php
+session_start();
+require '../../common/validation.php';
 require '../../common/database.php';
 
 $user_name = $_POST['user_name'];
 $user_email = $_POST['user_email'];
 $user_password = $_POST['user_password'];
+
+$_SESSION['errors'] = [];
+
+// 空チェック
+emptyCheck($_SESSION['errors'], $user_name, 'ユーザー名を入力してください。');
+emptyCheck($_SESSION['errors'], $user_email, 'メールアドレスを入力してください。');
+emptyCheck($_SESSION['errors'], $user_password, 'パスワードを入力してください。');
+
+// 文字数チェック
+stringMinSizeCheck($_SESSION['errors'], $user_password, 'パスワードは8文字以上で入力してください。');
+stringMaxSizeCheck($_SESSION['errors'], $user_name, 'ユーザー名は255文字以内で入力してください。');
+stringMaxSizeCheck($_SESSION['errors'], $user_email, 'メールアドレスは255文字以内で入力してください。');
+stringMaxSizeCheck($_SESSION['errors'], $user_password, 'パスワードは255文字以内で入力してください。');
+
+if(!$_SESSION['errors']) {
+  // メアドチェック
+  mailAddressCheck($_SESSION['errors'], $user_email, "正しいメールアドレスと入力してください。");
+  
+  // ユーザー名, パスワード半角英数チェック
+  halfAlphanumericCheck($_SESSION['errors'], $user_name, 'ユーザー名は半角英数字で入力してください。');
+  halfAlphanumericCheck($_SESSION['errors'], $user_password, 'パスワードは半角英数字で入力してください。');
+  
+  // メアド重複チェック
+  mailAddressDuplicationCheck($_SESSION['errors'], $user_email, 'すでに登録されているメールアドレスです。');
+}
+
+if($_SESSION['errors']) {
+  header('Location: ../../user/');
+  exit;
+}
 
 $database_handler = getDatabaseConnection();
 
