d2a8b22bd70243ec9aec4a2f20104145e88c3c2d
masanari9256_from_company <masanari92562@gmail.com>
2022-01-31 18:26:19 +0900

    [update]メモ削除機能を実装

---
+++ memo/action/delete.php
@@ -0,0 +1,31 @@
+<?php
+require '../../common/auth.php';
+require '../../common/database.php';
+
+if(!isLogin()) {
+  header('Location ../login/');
+  exit;
+}
+
+$edit_id = $_POST['edit_id'];
+$user_id = getLoginUserId();
+
+$database_handler = getDatabaseConnection();
+
+try {
+  if($statement = $database_handler->prepare(
+    'DELETE FROM memos WHERE id = :id AND user_id = :user_id'
+  )) {
+    $statement->bindParam(':id', $edit_id);
+    $statement->bindParam(':user_id', $user_id);
+    $statement->execute();
+  }
+} catch(Throwable $e) {
+  echo $e->getMessage();
+  exit;
+}
+
+unset($_SESSION['select_memo']);
+
+header('Location: ../../memo/');
+exit;
