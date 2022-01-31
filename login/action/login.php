d2e98e57509b7ba20fb5a552641ad43aca803168
masanari9256_from_company <masanari92562@gmail.com>
2022-01-31 19:09:10 +0900

    [updateログイン時に最新のメモを選択するようにした

--- login/action/login.php
+++ login/action/login.php
@@ -58,13 +58,32 @@ if($statement = $database_handler->prepare(
       'name' => $name,
       'id' => $id
     ];
+  
+    if($statement = $database_handler->prepare('
+        SELECT * FROM memos
+        WHERE user_id = :user_id
+        ORDER BY updated_at DESC LIMIT 1
+        ')) {
+      $statement->bindParam(':user_id', $id);
+      $statement->execute();
+    
+      $result = $statement->fetch(PDO::FETCH_ASSOC);
+    }
+  
+    if($result) {
+      $_SESSION['select_memo'] = [
+        'id' => $result['id'],
+        'title' => $result['title'],
+        'content' => $result['content']
+      ];
+    }
+    
     header('Location: ../../memo/');
-    exit;
   } else {
     $_SESSION['errors'] = [
       'メールアドレスまたはパスワードが間違っています。'
     ];
     header('Location: ../../login/');
-    exit;
   }
+  exit;
 }
